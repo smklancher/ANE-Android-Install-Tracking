fbe6436d2eda95f762c7d6f3407cf10253addf6a
Stephen Klancher <smknight@gmail.com>
2015-01-20 03:16:42 +0000

    skeleton build

---
+++ AndroidReferrer/AndroidReferrer/src/com/stephenklancher/androidreferrer/ReferrerReceiver.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.stephenklancher.androidreferrer;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * @author smkni_000
+ *
+ */
+public class ReferrerReceiver extends BroadcastReceiver {
+
+	/**
+	 * 
+	 */
+	public ReferrerReceiver() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see android.content.BroadcastReceiver#onReceive(android.content.Context, android.content.Intent)
+	 */
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
